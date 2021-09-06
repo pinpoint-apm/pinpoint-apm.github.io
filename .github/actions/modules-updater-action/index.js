e19edd3d3c936543f2a1f55149a68a9de3eff42e
Yongseok Kang <yongseok.kang@navercorp.com>
2021-09-06 22:09:04 +0900

    [#1] process.env

--- .github/actions/modules-updater-action/index.js
+++ .github/actions/modules-updater-action/index.js
@@ -15,6 +15,7 @@ try {
   console.log(`Hello ${templateMarkdownFile}!`)
   const time = (new Date()).toTimeString()
   core.setOutput("time", time)
+  console.log(`process.env: ${process.env}`)
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`)
