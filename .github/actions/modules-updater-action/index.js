b4241d2792e86c8d32c98f5eab47129b18059615
Yongseok Kang <yongseok.kang@navercorp.com>
2021-09-06 23:33:19 +0900

    [#1] remove setup node

--- .github/actions/modules-updater-action/index.js
+++ .github/actions/modules-updater-action/index.js
@@ -13,16 +13,16 @@ try {
   // `who-to-greet` input defined in action metadata file
   const templateMarkdownFile = core.getInput('template_markdown_file')
   console.log(`Hello ${templateMarkdownFile}!`)
-  const time = (new Date()).toTimeString()
-  core.setOutput("time", time)
 
   const env = JSON.stringify(process.env)
   console.log(`process.env: ${env}`)
 
-
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`)
+
+  const time = (new Date()).toTimeString()
+  core.setOutput("time", time)
 } catch (error) {
   core.setFailed(error.message)
 }
