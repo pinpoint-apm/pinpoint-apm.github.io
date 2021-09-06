971b47b8c75a23fec8fcf9c3ebf025bbed7efc0b
Yongseok Kang <yongseok.kang@navercorp.com>
2021-09-06 23:52:41 +0900

    [#1] aync function

--- .github/actions/modules-updater-action/index.js
+++ .github/actions/modules-updater-action/index.js
@@ -9,23 +9,27 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const templateMarkdownFile = core.getInput('template_markdown_file')
-  console.log(`Hello ${templateMarkdownFile}!`)
+const run = async () => {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const templateMarkdownFile = core.getInput('template_markdown_file')
+    console.log(`Hello ${templateMarkdownFile}!`)
 
-  const env = JSON.stringify(process.env)
-  console.log(`process.env: ${env}`)
+    const env = JSON.stringify(process.env)
+    console.log(`process.env: ${env}`)
 
-  const result = await fs.readFile(templateMarkdownFile, "utf-8")
-  console.log(`readfile result: ${result}`)
+    const result = await fs.readFile(templateMarkdownFile, "utf-8")
+    console.log(`readfile result: ${result}`)
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`)
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`)
 
-  const time = (new Date()).toTimeString()
-  core.setOutput("time", time)
-} catch (error) {
-  core.setFailed(error.message)
+    const time = (new Date()).toTimeString()
+    core.setOutput("time", time)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
 }
+
+run()
