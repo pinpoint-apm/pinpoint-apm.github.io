a8fa4745a68587b3ccc20ca39a8be91011bcbcec
Yongseok Kang <yongseok.kang@navercorp.com>
2021-09-07 17:15:40 +0900

    [#1] markdown content

--- .github/actions/modules-updater-action/index.js
+++ .github/actions/modules-updater-action/index.js
@@ -14,21 +14,11 @@ const run = async () => {
   try {
     // `who-to-greet` input defined in action metadata file
     const templateMarkdownFile = core.getInput('template_markdown_file')
-    console.log(`Hello ${templateMarkdownFile}`)
-
-    const env = JSON.stringify(process.env)
-    console.log(`process.env: ${env}`)
-
     const markdownContent = await fs.readFile(templateMarkdownFile, 'utf8')
     console.log(`readfile result: ${markdownContent}`)
+
+
     core.setOutput('data', markdownContent)
-
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`)
-
-    // const time = (new Date()).toTimeString()
-    // core.setOutput("time", time)
   } catch (error) {
     core.setFailed(error.message)
   }
