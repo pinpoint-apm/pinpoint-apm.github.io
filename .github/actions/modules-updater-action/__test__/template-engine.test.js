3458dd3824e62df6e43017186217bc0d44ee5422
Yongseok Kang <yongseok.kang@navercorp.com>
2021-09-08 20:24:29 +0900

    [#1] Test multiline case

--- .github/actions/modules-updater-action/__test__/template-engine.test.js
+++ .github/actions/modules-updater-action/__test__/template-engine.test.js
@@ -6,3 +6,50 @@
 
 'use strict'
 
+const test = require('tape')
+
+const mainMarkdownFile = `---
+title: Pinpoint 2.2.2
+keywords: 'pinpoint release, 2.2.2'
+permalink: main.html
+sidebar: mydoc_sidebar
+---
+
+# What's New
+
+## What's New in Pinpoint v2.0
+
+* Support gRPC tranport
+* Support for JAVA12, 13
+* Maximum throughput limit feature
+* renew UI
+
+[Releases](https://github.com/pinpoint-apm/pinpoint/releases)
+
+## Upgrade consideration
+
+HBase compatibility table:
+
+<!-- <compatibilityHbase.md> -->
+<!-- </compatibilityHbase.md> -->
+
+Agent compatibility to Collector table:
+
+<!-- <compatibilityPinpoint.md> -->
+<!-- </compatibilityPinpoint.md> -->
+
+Additionally, the required Java version to run each Pinpoint component is given below:
+
+<!-- <compatibilityJava.md> -->
+<!-- </compatibilityJava.md> -->
+## Supported Modules
+
+* JDK 6+
+* Supported versions of the \* indicated library may differ from the actual version.
+
+<!-- <modules.md> -->
+<!-- </modules.md> -->`
+
+test('tempalte engine', (t) => {
+
+})
