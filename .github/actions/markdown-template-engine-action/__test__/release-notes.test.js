694fb847f2bd15e1d40011866da602461642773e
Yongseok Kang <yongseok.kang@navercorp.com>
2021-10-21 23:57:08 +0900

    [#1] extrace method refactoring for tagname with or without 'v'

--- .github/actions/markdown-template-engine-action/__test__/release-notes.test.js
+++ .github/actions/markdown-template-engine-action/__test__/release-notes.test.js
@@ -15,3 +15,16 @@ test('release notes', async (t) => {
     t.true(/# What's New in/.test(actual), '`# What"s New in tagname` match error')
     t.end()
 })
+
+test('tagname check with or without "v"', async (t) => {
+    const dut = new MarkdownContents()
+    dut.getLatestReleaseNotes = async () => {
+        return {
+            tag_name: 'v1.0',
+            body: ''
+        }
+    }
+    const actual = await dut.markdownContentsFromPinpointLatestReleaseNotes()
+    t.true(/# What's New in v1.0/.test(actual), '`# What"s New in tagname with "v"')
+    t.end()
+})
